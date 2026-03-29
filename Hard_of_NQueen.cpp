67ee3c6379871bc60f8cd47a4bdbd4802c01ac8c
Veepin_Kumar <veepin8459@gmail.com>
2026-03-29 18:29:00 +0530

    correct

--- Hard_of_NQueen.cpp
+++ Hard_of_NQueen.cpp
@@ -1,66 +1,74 @@
+//
+//  N_Queen.cpp
+//  DSA_with_Cpp
+//
+//  Created by Veepin_Chaudhary on 28/03/26.
+//
+
 #include <iostream>
 #include <vector>
-
+#include <string>
 using namespace std;
-
 class Solution {
 public:
-
-
-     void solve(int col,vector<vector<string>> & ans,vector<string> & res,vector<int> & leftRow,vector<int> & lowerDiagonal,vector<int> &upperDiagonal,int n){
-        if(col == n) {
-            ans.push_back(res);
-            return ;
-
+    
+   
+    
+    void slice(int col,vector<string> & board,vector<vector<string>> & ans,vector<int> & leftrow,vector<int> & lowerdiagonal,vector<int> &upperdiagonal,int N){
+        if(col==N){
+            ans.push_back(board);
+            return;
         }
-        for(int row =0 ; row<n;row++){
-            if(leftRow[row]==0 &&  lowerDiagonal[row + col]==0 && upperDiagonal[n-1 + col -row]==0){
-                res[row][col]='Q';
-                leftRow[row]=1;
-                lowerDiagonal[row]=1;
-                upperDiagonal[row]=1;
-                solve(col+1,ans,res,leftRow,lowerDiagonal,upperDiagonal,n);
-                res[row]='.';
-                leftRow[row]=0;
-                lowerDiagonal[row+col]=0;
-                upperDiagonal[n-1 + col - row]=0;
-                
+        for(int row=0;row<N;row++){
+            if(leftrow[row]==0 && lowerdiagonal[row + col] == 0 && upperdiagonal[N-1 + col -row]==0){
+                board[row][col]='Q';
+                leftrow[row]=1;
+                lowerdiagonal[row + col]=1;
+                upperdiagonal[N-1 + col - row]=1;
+
+                slice(col+1,board,ans,leftrow,lowerdiagonal,upperdiagonal,N);
+                board[row][col]='.';
+                leftrow[row]=0;
+                lowerdiagonal[row + col]=0;
+                upperdiagonal[N-1 + col - row]=0;
+
             }
+            
         }
-
-
-     }
-
-    vector<vector<string>> nQueen(int n){
-        vector<vector<string>> ans;
-        vector<string> res(n);
-        string s(n,'.');
-
-        for(int row=0;row<n;row++){
-            res[row] = s;
-        }
-        vector<int> leftRow(n,0);
-        vector<int> lowerDiagonal(2*n-1,0);
-        vector<int> upperDaigonal(2*n-1,0);
-
-        solve(0,ans,res,leftRow,lowerDiagonal,upperDaigonal,n);
-        return ans;
-
+        
     }
-
+    
+    vector<vector<string>> Que(int N) {
+        vector<vector<string>> ans;
+        vector<string> board(N);
+        string s(N,'.');
+        for(int i=0;i<N;i++){
+            board[i]=s;
+        }
+        vector<int> leftrow(N,0);
+        vector<int> lowerdiagonal(2*N-1,0);
+        vector<int> upperdiagonal(2*N-1,0);
+        
+        slice(0,board,ans,leftrow,lowerdiagonal,upperdiagonal,N);
+        return ans;
+    }
+    
 };
-int main(){
 
+int main(){
     Solution S;
-    int n;
-    cin>>n;
-    vector<vector<string>> result=S.nQueen(n);
-    for(auto row: result){
+    int N;
+    cin>>N;
+    vector<vector<string>> Queen=S.Que(N);
+    for(auto row : Queen){
         for(auto ele : row){
             cout<<ele<<" ";
         }
         cout<<endl;
     }
+    cout<<endl;
     return 0;
-
+    
 }
+
+
